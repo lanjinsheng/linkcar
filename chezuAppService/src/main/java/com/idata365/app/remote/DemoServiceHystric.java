031531a8e0e5d6ff6602a48d222c4faff8264364
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-11-21 02:52:05 +0000

    lanjinsheng  add demo

--- chezuAppService/src/main/java/com/idata365/app/remote/DemoServiceHystric.java
+++ chezuAppService/src/main/java/com/idata365/app/remote/DemoServiceHystric.java
@@ -1,6 +1,10 @@
 package com.idata365.app.remote;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
+
+import com.idata365.app.entity.UserEntity;
 
 @Component
 public class DemoServiceHystric implements DemoService {
@@ -13,6 +17,17 @@ public class DemoServiceHystric implements DemoService {
 	public String getUsers() {
 		// TODO Auto-generated method stub
 		return "sorry,sorry getUsers ";
+	}
+
+	@Override
+	public void save(UserEntity user) {
+		// TODO Auto-generated method stub
+//		return "sorry,sorry save ";
+	}
+	@Override
+	public String saveByMap(Map<Object, Object> user) {
+		// TODO Auto-generated method stub
+		return "sorry,sorry saveByMap ";
 	}
 
 }
