7a9cf5ece8e0baae8394ee29a0a77da4ec600107
wangxiaoming <wangxiaoming@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-09 06:48:45 +0000

    签到接口

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/controller/BaseController.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/controller/BaseController.java
@@ -3,12 +3,17 @@ package com.idata365.app.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import com.idata365.app.entity.bean.UserInfo;
 
 abstract public class BaseController {
+	
+	protected static final Logger LOG = LoggerFactory.getLogger(BaseController.class);
+	
 	protected UserInfo getUserInfo() {
 		   RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
 	  	   HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
