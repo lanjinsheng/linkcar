92a676272ca265a993a23d28783e010f55e91b35
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-13 07:52:08 +0000

    lanjinsheng add

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/LicenseVehicleTravelMapper.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/LicenseVehicleTravelMapper.java
@@ -2,9 +2,15 @@ package com.idata365.app.mapper;
 
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.idata365.app.entity.LicenseDriver;
+import com.idata365.app.entity.LicenseVehicleTravel;
+
 public interface LicenseVehicleTravelMapper {
 
 	 void  insertImgVehicleFrontImg(Map<String,Object> xsz);
 	 void  modifyImgVehicleFront(Map<String,Object> xsz);
 	 void  insertImgVehicleBackImg(Map<String,Object> xsz);
+	 LicenseVehicleTravel  findLicenseVehicleTravelByUserId(@Param("userId")  Long userId);
 }
