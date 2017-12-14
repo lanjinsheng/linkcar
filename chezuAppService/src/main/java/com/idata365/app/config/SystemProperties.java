21aea844ce86730ba03292f4059f2bf0f2f1fbed
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-14 12:45:01 +0000

    lanjinsheng add

--- chezuAppService/src/main/java/com/idata365/app/config/SystemProperties.java
+++ chezuAppService/src/main/java/com/idata365/app/config/SystemProperties.java
@@ -3,14 +3,41 @@ package com.idata365.app.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.idata365.app.partnerApi.SSOTools;
 @Configuration
 @PropertySource("classpath:system.properties")
 @ConfigurationProperties(prefix = "system")
 public class SystemProperties {
+	public SystemProperties() {
+		
+	}
 	private String fileTmpDir;
     private String jgAppKey;
     private String jgSecret;
+    private String product;
+    private  String ssoAccessKeyId;
+    private  String ssoAccessKeySecret;
     
+	public String getSsoAccessKeyId() {
+		return ssoAccessKeyId;
+	}
+
+	public void setSsoAccessKeyId(String ssoAccessKeyId) {
+		this.ssoAccessKeyId = ssoAccessKeyId;
+		SSOTools.accessKeyId=ssoAccessKeyId;
+	}
+
+	public String getSsoAccessKeySecret() {
+		return ssoAccessKeySecret;
+		
+	}
+
+	public void setSsoAccessKeySecret(String ssoAccessKeySecret) {
+		this.ssoAccessKeySecret = ssoAccessKeySecret;
+		SSOTools.accessKeySecret=ssoAccessKeySecret;
+	}
+
 	public String getFileTmpDir() {
 		return fileTmpDir;
 	}
@@ -33,6 +60,15 @@ public class SystemProperties {
 
 	public void setJgSecret(String jgSecret) {
 		this.jgSecret = jgSecret;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+		
 	}
 	
 	
