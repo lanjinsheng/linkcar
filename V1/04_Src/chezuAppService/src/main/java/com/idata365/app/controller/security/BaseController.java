77f8ce8d706cd7deddeeaf78608de1db52332ec3
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-15 06:30:27 +0000

    lanjinsheng add

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/controller/security/BaseController.java
@@ -0,0 +1,29 @@
+package com.idata365.app.controller.security;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import com.idata365.app.entity.bean.UserInfo;
+
+abstract  class BaseController {
+	
+	protected static final Logger LOG = LoggerFactory.getLogger(BaseController.class);
+	
+	protected UserInfo getUserInfo() {
+		   RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+	  	   HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
+	  	   UserInfo userInfo=(UserInfo)  request.getAttribute("userInfo");
+		   return userInfo;
+	}
+	protected Long getUserId() {
+		   RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+	  	   HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
+	  	   UserInfo userInfo=(UserInfo)  request.getAttribute("userInfo");
+		   return userInfo.getId();
+	}
+}
