5de82909afaed062aff09a60c4888fe9ded51753
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-15 10:07:17 +0000

    lanjinsheng add message

---
+++ chezuAppService/src/main/java/com/idata365/app/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.idata365.app.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+
+abstract  class BaseController {
+	
+	protected static final Logger LOG = LoggerFactory.getLogger(BaseController.class);
+	
+	 
+	protected String getFamilyInviteBasePath() {
+		  RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+	   	     HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
+	   	     return "http://"+request.getServerName()+":"+request.getServerPort()+"/share/goInvite?key=";
+
+	  }
+}
