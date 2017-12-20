a0ab02e35231a6cb5d126043719813ad7cbf5c91
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-20 07:44:33 +0000

    lanjinsheng add

--- chezuAppService/src/main/java/com/idata365/app/controller/ImgsInfoController.java
+++ chezuAppService/src/main/java/com/idata365/app/controller/ImgsInfoController.java
@@ -1,7 +1,11 @@
 package com.idata365.app.controller;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,40 +26,40 @@ import com.idata365.app.config.SystemProperties;
 import com.idata365.app.partnerApi.SSOTools;
 
  
-@RestController
+@Controller
 public class ImgsInfoController {
 	private final static Logger LOG = LoggerFactory.getLogger(ImgsInfoController.class);
     @Autowired
 	SystemProperties systemProperties;
 	
-	@RequestMapping(value = "/userFiles/getImgs",  method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json;charset=UTF-8")
-    public ResponseEntity<InputStreamResource> getImgs(@RequestParam (required = false) Map<String, String> allRequestParams,@RequestBody  (required = false)  Map<Object, Object> requestBodyParams){
+	@RequestMapping(value = "/userFiles/getImgs")
+    public void  getImgs(@RequestParam (required = false) Map<String, String> allRequestParams,HttpServletResponse response){
 	    LOG.info("start");
 		  String  key=allRequestParams.get("key");
+//		  key=key.split(".")[0];
 	        HttpHeaders headers = new HttpHeaders();  
 	        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");  
-	        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", key));  
+	        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", key+".jpeg"));  
 	        headers.add("Pragma", "no-cache");  
 	        headers.add("Expires", "0");  
-	        FileSystemResource   file = new FileSystemResource(systemProperties.getFileTmpDir()+key.replaceAll("/", "")+".jpg");
+	        FileSystemResource   file = new FileSystemResource(systemProperties.getFileTmpDir()+key.replaceAll("/", "")+".jpeg");
 	        
 	         try {
 	        	 OutputStream os=file.getOutputStream();
 	        	 SSOTools.getSSOFile(key,os);
-	        	 
-		        return ResponseEntity  
-		                .ok().contentLength(file.contentLength())
-		                .headers(headers)  
-		                .contentType(MediaType.parseMediaType("image/jpeg"))  
-		                .body(new InputStreamResource(file.getInputStream())); 
+	        	 InputStream inputStream = file.getInputStream();
+	             byte[] data = new byte[(int)file.contentLength()];
+	             int length = inputStream.read(data);
+	             inputStream.close();
+	             response.setContentType("image/jpeg");
+
+	             OutputStream stream = response.getOutputStream();
+	             stream.write(data);
+	             stream.flush();
+	             stream.close();
 			  }catch(Exception e) {
 				  e.printStackTrace();
 				  //放张未知图片
-				  return ResponseEntity  
-			                .ok().contentLength(0)
-			                .headers(headers)  
-			                .contentType(MediaType.parseMediaType("image/jpeg"))  
-			                .body(null); 
 			  }finally{
 		        
 		      }
