8db8a33ea43cd406356de083eeda909de115549d
wangxiaoming <wangxiaoming@ba625775-5f01-0010-890b-93b6b773ee2d>
2017-12-22 07:49:36 +0000

    【成绩管理】【历史驾驶得分】

---
+++ chezuAppService/src/main/java/com/idata365/app/entity/ScoreByDayBean.java
@@ -0,0 +1,77 @@
+package com.idata365.app.entity;
+
+public class ScoreByDayBean
+{
+	private double score;
+	
+	private double speedingState;
+	
+	private double driveState;
+	
+	private double creditState;
+	
+	private double steadyState;
+	
+	private double mileageState;
+
+	public double getScore()
+	{
+		return score;
+	}
+
+	public void setScore(double score)
+	{
+		this.score = score;
+	}
+
+	public double getSpeedingState()
+	{
+		return speedingState;
+	}
+
+	public void setSpeedingState(double speedingState)
+	{
+		this.speedingState = speedingState;
+	}
+
+	public double getDriveState()
+	{
+		return driveState;
+	}
+
+	public void setDriveState(double driveState)
+	{
+		this.driveState = driveState;
+	}
+
+	public double getCreditState()
+	{
+		return creditState;
+	}
+
+	public void setCreditState(double creditState)
+	{
+		this.creditState = creditState;
+	}
+
+	public double getSteadyState()
+	{
+		return steadyState;
+	}
+
+	public void setSteadyState(double steadyState)
+	{
+		this.steadyState = steadyState;
+	}
+
+	public double getMileageState()
+	{
+		return mileageState;
+	}
+
+	public void setMileageState(double mileageState)
+	{
+		this.mileageState = mileageState;
+	}
+
+}
---
+++ chezuAppService/src/main/java/com/idata365/app/entity/ScoreByDayResultBean.java
@@ -0,0 +1,77 @@
+package com.idata365.app.entity;
+
+public class ScoreByDayResultBean
+{
+	private String score;
+	
+	private String speedingState;
+	
+	private String driveState;
+	
+	private String creditState;
+	
+	private String steadyState;
+	
+	private String mileageState;
+
+	public String getScore()
+	{
+		return score;
+	}
+
+	public void setScore(String score)
+	{
+		this.score = score;
+	}
+
+	public String getSpeedingState()
+	{
+		return speedingState;
+	}
+
+	public void setSpeedingState(String speedingState)
+	{
+		this.speedingState = speedingState;
+	}
+
+	public String getDriveState()
+	{
+		return driveState;
+	}
+
+	public void setDriveState(String driveState)
+	{
+		this.driveState = driveState;
+	}
+
+	public String getCreditState()
+	{
+		return creditState;
+	}
+
+	public void setCreditState(String creditState)
+	{
+		this.creditState = creditState;
+	}
+
+	public String getSteadyState()
+	{
+		return steadyState;
+	}
+
+	public void setSteadyState(String steadyState)
+	{
+		this.steadyState = steadyState;
+	}
+
+	public String getMileageState()
+	{
+		return mileageState;
+	}
+
+	public void setMileageState(String mileageState)
+	{
+		this.mileageState = mileageState;
+	}
+	
+}
