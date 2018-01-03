ef38808e44473ed1e97ff313248a25ad157fe7b7
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-03 10:13:57 +0000

    lanjinsheng  提交远程获取数据接口

--- chezuColService/src/main/java/com/idata365/col/remote/ChezuServiceHystric.java
+++ chezuColService/src/main/java/com/idata365/col/remote/ChezuServiceHystric.java
@@ -1,6 +1,5 @@
 package com.idata365.col.remote;
 
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -10,23 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class ChezuServiceHystric implements ChezuDriveService {
 	private final static Logger LOG = LoggerFactory.getLogger(ChezuServiceHystric.class);
+
 	@Override
-	public Map<String, Object> addPoint(Map<String, String> point) {
+	public Map<String, Object> getScoreByUH(Long userId, Long habitId, String sign) {
 		// TODO Auto-generated method stub
-		LOG.error("挂了 addPoint");
+		LOG.info("ChezuServiceHystric 调用错误");
 		return null;
 	}
-	@Override
-	public Map<String, Object> addPointList(List<Map<String, String>> pointList) {
-		// TODO Auto-generated method stub
-		LOG.error("挂了 addPointList");
-		return null;
-	}
-	@Override
-	public Map<String, Object> analysis(Map<String, String> param) {
-		// TODO Auto-generated method stub
-		LOG.error("挂了 analysis");
-		return null;
-	}
+	 
  
 }
