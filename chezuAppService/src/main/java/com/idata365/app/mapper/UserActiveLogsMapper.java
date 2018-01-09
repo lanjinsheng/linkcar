7c189715b8059204e2a1c7ee59a165a815d29cd9
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-09 05:08:27 +0000

    lanjinsheng  增加日分统计

--- chezuAppService/src/main/java/com/idata365/app/mapper/UserActiveLogsMapper.java
+++ chezuAppService/src/main/java/com/idata365/app/mapper/UserActiveLogsMapper.java
@@ -4,4 +4,5 @@ import com.idata365.app.entity.UserActiveLogs;
 
 public interface UserActiveLogsMapper {
 	public void insertUserActiveLogs(UserActiveLogs userActiveLogs);
+	public void updateUserFamilyActiveLogs(UserActiveLogs userActiveLogs);
 }
