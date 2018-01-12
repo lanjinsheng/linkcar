d6940acdba4aee239e5ee49a5656e2908d48241b
wangxiaoming <wangxiaoming@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-12 11:40:43 +0000

    /score/gameHistory 赛季历程（一个自然月为一个周期）1实现

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/entity/GameHistoryBean.java
@@ -0,0 +1,29 @@
+package com.idata365.app.entity;
+
+public class GameHistoryBean
+{
+	private int orderNo;
+	
+	private String daystamp;
+
+	public int getOrderNo()
+	{
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo)
+	{
+		this.orderNo = orderNo;
+	}
+
+	public String getDaystamp()
+	{
+		return daystamp;
+	}
+
+	public void setDaystamp(String daystamp)
+	{
+		this.daystamp = daystamp;
+	}
+	
+}
