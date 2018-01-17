26aa6b31e34ed9b3d12ae06264acbc48a13bff8b
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-17 07:51:50 +0000

    清除无效引入包名

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/GameService.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/GameService.java
@@ -17,8 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.yaml.snakeyaml.introspector.BeanAccess;
-
 import com.alibaba.fastjson.JSON;
 import com.idata365.app.constant.DateConstant;
 import com.idata365.app.constant.FamilyConstant;
