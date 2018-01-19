3665f670da452d5a8b81d093b8c3d7f7404c56f9
caifengyao <caifengyao@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-19 09:35:38 +0000

    新增个人成就

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/UserAchieveService.java
@@ -0,0 +1,43 @@
+package com.idata365.app.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.idata365.app.mapper.UserAchieveMapper;
+
+@Service
+public class UserAchieveService
+{
+	private static final Logger LOG = LoggerFactory.getLogger(UserAchieveService.class);
+
+	@Autowired
+	private UserAchieveMapper userAchieveMapper;
+
+	/**
+	 * 查询个人成就列表
+	 * 
+	 * @Description:TODO 查询语句有待修正
+	 * @author:CaiFengYao
+	 * @date:2018年1月19日 上午11:05:26
+	 */
+	public List<Map<String, Object>> getUserAchieveList(long userId)
+	{
+		// 查询列表
+		return userAchieveMapper.getUserAchieveList(userId);
+	}
+
+	public List<Map<String, Object>> getAchieveListById(long userId, int achieveId)
+	{
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("userId", userId);
+		map.put("achieveId", achieveId);
+		// 查询列表
+		return userAchieveMapper.getUserAchieveListById(map);
+	}
+}
