d1deeab3fd60ad68172b41c6233eebaf338595a3
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-01-29 11:49:23 +0000

    增加成就定时任务

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/schedule/ClearLockTask.java
@@ -0,0 +1,53 @@
+package com.idata365.app.schedule;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.idata365.app.service.TaskAchieveAddValueService;
+
+
+public class ClearLockTask extends TimerTask { 
+	private static Logger log = Logger.getLogger(ClearLockTask.class);
+	private static Object lock = new Object();
+	public static boolean pd=true;
+	
+  //注入ThreadPoolTaskExecutor 到主线程中  
+	private ThreadPoolTaskExecutor threadPool;  
+    @Autowired
+    TaskAchieveAddValueService taskAchieveAddValueService;
+ 
+    
+	public void setThreadPool(ThreadPoolTaskExecutor threadPool){  
+//		System.out.println(new Date().getTime());
+	 this.threadPool = threadPool;  
+	}  
+
+	//在主线程中执行任务线程.....    
+	@Override  
+	public void run() {  
+		log.info("ClearLockTask start--");
+ 
+		if(!pd){
+			return;
+		}
+		synchronized (lock){
+		if(pd){
+			pd=false;
+		
+			try {
+				taskAchieveAddValueService.clearLockTask();
+				 
+			}catch(Exception e) {
+				e.printStackTrace();
+				log.error(e);
+				
+			}
+			pd=true;
+		}
+			
+		}
+		log.info("ClearLockTask end--");
+	}  
+}
