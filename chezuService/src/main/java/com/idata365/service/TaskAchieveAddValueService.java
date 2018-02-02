e1ad40eb7e29d0668e0e8b67e83ee64114c5fb87
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-02-02 02:57:01 +0000

    修改成就bug

--- chezuService/src/main/java/com/idata365/service/TaskAchieveAddValueService.java
+++ chezuService/src/main/java/com/idata365/service/TaskAchieveAddValueService.java
@@ -2,6 +2,7 @@ package com.idata365.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.idata365.entity.TaskAchieveAddValue;
 import com.idata365.enums.AchieveEnum;
