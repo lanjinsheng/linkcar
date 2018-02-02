11de0c0eff75f8b73e79a8dce22abc289f1b8032
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-02-02 07:48:11 +0000

    改造开奖消息

--- chezuAppService/src/main/java/com/idata365/app/controller/H5Controller.java
+++ chezuAppService/src/main/java/com/idata365/app/controller/H5Controller.java
@@ -6,12 +6,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.idata365.app.entity.AwardResultBean;
 import com.idata365.app.service.AwardService;
 import com.idata365.app.util.ResultUtils;
+import com.idata365.app.util.StringTools;
 
 @RestController
 public class H5Controller extends BaseController
@@ -22,9 +25,14 @@ public class H5Controller extends BaseController
 	private AwardService awardService;
 	
 	@RequestMapping("/h5/showAwardUser")
-	public Map<String, Object> showAwardUser()
+	public Map<String, Object> showAwardUser(@RequestParam (required = false) Map<String, String> allRequestParams,@RequestBody  (required = false)  Map<Object, Object> requestBodyParams)
 	{
-		AwardResultBean resultBean = this.awardService.showAwardUser();
+		AwardResultBean resultBean =new AwardResultBean();
+		if(allRequestParams.get("id")!=null && !allRequestParams.get("id").equals("")) {
+			resultBean = this.awardService.showAwardUser(Long.valueOf(allRequestParams.get("id")));
+		}else {
+			resultBean = this.awardService.showAwardUser(0L);
+		}
 		String imgBasePath = super.getImgBasePath();
 		String imgUrl = resultBean.getImgUrl();
 		if (StringUtils.isNotBlank(imgUrl))
