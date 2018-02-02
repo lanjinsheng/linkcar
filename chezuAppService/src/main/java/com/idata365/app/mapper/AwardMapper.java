aee4fb7461851f7ae84620ef2743cfedb3683d1c
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-02-02 07:48:23 +0000

    改造开奖消息

--- chezuAppService/src/main/java/com/idata365/app/mapper/AwardMapper.java
+++ chezuAppService/src/main/java/com/idata365/app/mapper/AwardMapper.java
@@ -2,6 +2,8 @@ package com.idata365.app.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.idata365.app.entity.AwardBean;
 import com.idata365.app.entity.QuestionBean;
 import com.idata365.app.entity.QuestionParamBean;
@@ -9,6 +11,8 @@ import com.idata365.app.entity.QuestionParamBean;
 public interface AwardMapper
 {
 	public AwardBean queryAwardInfo();
+	
+	public AwardBean queryAwardInfoById(@Param("id") Long msgId);
 	
 	public List<QuestionBean> queryQuestions(QuestionParamBean bean);
 }
