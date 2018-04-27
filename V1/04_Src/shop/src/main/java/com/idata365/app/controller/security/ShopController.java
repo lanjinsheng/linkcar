a08b3640fef4b83482ee158f3910419cd9148aa6
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-04-27 07:13:25 +0000

    lan init

--- V1/04_Src/shop/src/main/java/com/idata365/app/controller/security/ShopController.java
+++ V1/04_Src/shop/src/main/java/com/idata365/app/controller/security/ShopController.java
@@ -1,8 +1,18 @@
 package com.idata365.app.controller.security;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.idata365.app.util.ResultUtils;
 
 @RestController
 public class ShopController extends BaseController{
-
+	 @RequestMapping("/test/doTest")
+	 public Map<String,Object> doTest(@RequestParam (required = false) Map<String, String> allRequestParams,@RequestBody  (required = false)  Map<Object, Object> requestBodyParams){
+	     return ResultUtils.rtSuccess(null);
+	 }
 }
