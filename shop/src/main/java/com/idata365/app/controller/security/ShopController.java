db730ed5adf1ca5e5e5523a0ddf4d8e8db022adf
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-04-27 13:53:12 +0000

    lan add 资产获取

--- shop/src/main/java/com/idata365/app/controller/security/ShopController.java
+++ shop/src/main/java/com/idata365/app/controller/security/ShopController.java
@@ -2,17 +2,25 @@ package com.idata365.app.controller.security;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.idata365.app.remote.ChezuAssetService;
 import com.idata365.app.util.ResultUtils;
+import com.idata365.app.util.SignUtils;
 
 @RestController
 public class ShopController extends BaseController{
+	 @Autowired
+	 ChezuAssetService chezuAssetService;
 	 @RequestMapping("/test/doTest")
 	 public Map<String,Object> doTest(@RequestParam (required = false) Map<String, String> allRequestParams,@RequestBody  (required = false)  Map<Object, Object> requestBodyParams){
-	     return ResultUtils.rtSuccess(null);
+		 long userId=this.getUserId();
+		 String sign=SignUtils.encryptHMAC(String.valueOf(userId));
+		 Map<String,Object> map=chezuAssetService.getUserAsset(userId, sign);
+		 return ResultUtils.rtSuccess(map);
 	 }
 }
