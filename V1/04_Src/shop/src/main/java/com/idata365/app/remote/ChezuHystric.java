b976bbc8859abce3f06b6e96492d0798d389aedb
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-04-27 02:36:19 +0000

    lan init

--- V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuHystric.java
+++ V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuHystric.java
@@ -1,6 +1,5 @@
 package com.idata365.app.remote;
 
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,19 +11,6 @@ import com.idata365.app.entity.UsersAccount;
 @Component
 public class ChezuHystric implements ChezuService {
 	private final static Logger LOG = LoggerFactory.getLogger(ChezuHystric.class);
-	@Override
-	public boolean updateUserDevice( Map<String,Object> map) {
-		// TODO Auto-generated method stub
-		LOG.info("updateUserDevice 挂了 service-chezu");
-		return false;
-	}
-	
-	@Override
-	public Map<String, Object> getGpsByUH(Map<String,Object> map) {
-		// TODO Auto-generated method stub
-		LOG.info(" getGpsByUH 挂了   service-chezu");
-		return null;
-	}
 
 	@Override
 	public UsersAccount validToken(String token) {
