11c94d99f57ded327c3214c62ef4f337bf998f06
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-04-27 02:36:19 +0000

    lan init

--- shop/src/main/java/com/idata365/app/remote/ChezuService.java
+++ shop/src/main/java/com/idata365/app/remote/ChezuService.java
@@ -20,30 +20,14 @@ import com.idata365.app.entity.UsersAccount;
  */
 @FeignClient(value = "service-chezu",fallback = ChezuHystric.class)
 public interface ChezuService {
-    @RequestMapping(value = "/v1/updateUserDevice",method = RequestMethod.POST)
-    boolean updateUserDevice(@RequestParam  Map<String,Object>  map);
-    
-    /**
-     * 
-        * @Title: getGpsByUH
-        * @Description: TODO(sign是通过SignUtils.encryptHMAC(userId+habitId)所得)
-        * @param  map 里放 userId  habitId  sign
-        * @param @param userId
-        * @param @param habitId
-        * @param @param sign
-        * @param @return    参数
-        * @return Map<String,Object>    返回类型
-        * 	  Map<String,Object> datas=new HashMap<String,Object>();
-	    *	  datas.put("gpsInfo", list);
-	    *	  datas.put("alarmInfo", events);
-        * @throws
-        * @author LanYeYe
-     */
-   @RequestMapping(value = "/drive/getGpsByUH",method = RequestMethod.POST)
-   Map<String,Object> getGpsByUH(@RequestParam  Map<String,Object>  map);
-   
-   
+   /**
+    * 远程进行用户账户验证，待远程接口写入
+    * @param token
+    * @return
+    */
    @RequestMapping(value = "/Account/validToken",method = RequestMethod.POST)
    UsersAccount  validToken(@RequestParam  String  token);
+   
+
    
 }
