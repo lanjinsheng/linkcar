7a8e1bebec9680bc756680323083e2f79e1848a9
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-03 08:03:20 +0000

    lan add 动力增加

---
+++ asset/src/main/java/com/idata365/app/entity/FamilyGameAsset.java
@@ -0,0 +1,72 @@
+package com.idata365.app.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 
+    * @ClassName: FamilyGameAsset
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author LanYeYe
+    * @date 2018年4月27日
+    *
+ */
+public class FamilyGameAsset implements Serializable {
+	
+	
+	    /**
+	    * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
+	    */
+	private static final long serialVersionUID = -6795900371476785023L;
+	private Long id;
+	private Long familyId;
+	private String startDay;
+	private String endDay;
+	private Long orderNo;
+	private BigDecimal diamondsNum;
+	private String seasonName;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getFamilyId() {
+		return familyId;
+	}
+	public void setFamilyId(Long familyId) {
+		this.familyId = familyId;
+	}
+	public String getStartDay() {
+		return startDay;
+	}
+	public void setStartDay(String startDay) {
+		this.startDay = startDay;
+	}
+	public String getEndDay() {
+		return endDay;
+	}
+	public void setEndDay(String endDay) {
+		this.endDay = endDay;
+	}
+	public Long getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(Long orderNo) {
+		this.orderNo = orderNo;
+	}
+	public BigDecimal getDiamondsNum() {
+		return diamondsNum;
+	}
+	public void setDiamondsNum(BigDecimal diamondsNum) {
+		this.diamondsNum = diamondsNum;
+	}
+	public String getSeasonName() {
+		return seasonName;
+	}
+	public void setSeasonName(String seasonName) {
+		this.seasonName = seasonName;
+	}
+	
+	
+}
