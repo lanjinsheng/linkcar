e0224878df4288435724c0f6afb5a47dbd19df61
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-03 09:07:35 +0000

    lan add 用户信息获取

---
+++ V1/04_Src/account/src/main/java/com/idata365/app/service/AccountService.java
@@ -0,0 +1,112 @@
+package com.idata365.app.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+    * @ClassName: CollectService
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author LanYeYe
+    * @date 2017年11月23日
+    *
+ */
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.idata365.app.entity.UserLoginSession;
+import com.idata365.app.entity.UsersAccount;
+import com.idata365.app.mapper.UserLoginSessionMapper;
+import com.idata365.app.mapper.UsersAccountMapper;
+import com.idata365.app.util.DateTools;
+ 
+
+@Service
+public class AccountService extends BaseService<AccountService>
+{
+	private final static Logger LOG = LoggerFactory.getLogger(AccountService.class);
+	@Autowired
+	UsersAccountMapper usersAccountMapper;
+	@Autowired
+	UserLoginSessionMapper userLoginSessionMapper;
+
+	 /**
+	  * 
+	     * @Title: getFamiliesInfoByUserId
+	     * @Description: TODO(这里用一句话描述这个方法的作用)
+	     * @param @param userIds
+	     * @param @return    参数
+	     * @return Map<String,Object>    返回类型
+	     * @throws
+	     * @author LanYeYe
+	  */
+	@Transactional
+	public Map<String,Object> getFamiliesInfoByUserId(String userIds)
+	{
+		Map<String,Object> rtMap=new HashMap<String,Object>();
+		StringBuffer userNickNames=new StringBuffer();
+		StringBuffer userHeadUrls=new StringBuffer();
+
+		String []users=userIds.split(",");
+		for(String id:users) {
+			UsersAccount account=usersAccountMapper.findAccountById(Long.valueOf(id));
+			if(account!=null) {
+				userNickNames.append(account.getNickName()==null?account.getPhone():account.getNickName());
+				userNickNames.append(",");
+				userHeadUrls.append(account.getImgUrl()==null?"":account.getImgUrl());
+				userHeadUrls.append(",");
+			}
+		}
+		rtMap.put("nickNames", userNickNames.substring(0, userNickNames.length()-1));
+		rtMap.put("userHeadUrls", userHeadUrls.substring(0, userHeadUrls.length()-1));
+		return rtMap;
+	}
+	
+	/**
+	 * 
+	    * @Title: getFamiliesInfoByUserId
+	    * @Description: TODO(这里用一句话描述这个方法的作用)
+	    * @param @param userId
+	    * @param @return    参数
+	    * @return Map<String,Object>    返回类型
+	    * @throws
+	    * @author LanYeYe
+	 */
+	@Transactional
+	public Map<String,Object> getFamiliesInfoByUserId(long userId)
+	{
+		Map<String,Object> rtMap=new HashMap<String,Object>();
+		 Map<String,Object> my=usersAccountMapper.getFamilyByUserId(userId);
+//	 
+//		    * Map.put("fightFamilyId":"id")
+//	        * Map.put("fightFamilyUserCount":"number")
+		   rtMap.put("familyId", my.get("id"));
+	       rtMap.put("familyUserCount", my.get("memberNum"));
+	       rtMap.put("daystamp", DateTools.getCurDateYYYY_MM_dd());
+	       Map<String,Object> opp=usersAccountMapper.getOpponentFamilyId(rtMap);
+	       
+	        if(opp!=null && opp.size()>0) {
+	        	Long familyId2=0l;
+	        	if(String.valueOf(opp.get("selfFamilyId")).equals(String.valueOf(my.get("id")))){
+	        		familyId2=Long.valueOf(opp.get("competitorFamilyId").toString());
+	        	}else {
+	        		familyId2=Long.valueOf(opp.get("selfFamilyId").toString());
+	        	}
+	        	 Map<String,Object> family2=usersAccountMapper.getFamilyByFamilyId(familyId2);
+	        	 rtMap.put("fightFamilyId",family2.get("id"));
+	  	         rtMap.put("fightFamilyUserCount", family2.get("memberNum"));
+	        }else {
+	        	 rtMap.put("fightFamilyId",0);
+	  	         rtMap.put("fightFamilyUserCount", 0);
+	        }
+	        
+		return rtMap;
+	}
+	
+}
