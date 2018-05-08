a36aeda482c1a960594cd55a972cb5da979b21df
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-08 05:49:25 +0000

    lan add 兑换消息发送

--- V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuAppService.java
+++ V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuAppService.java
@@ -1,12 +1,9 @@
 package com.idata365.app.remote;
 
-import java.util.Map;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.idata365.app.entity.UsersAccount;
 
 /**
  * 
