3576ab8e770f0229338493e6d49ac39390356f82
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-09 15:50:42 +0000

    lixing

--- chezuAppService/src/main/java/com/idata365/app/partnerApi/PhoneMsgTools.java
+++ chezuAppService/src/main/java/com/idata365/app/partnerApi/PhoneMsgTools.java
@@ -1,8 +1,31 @@
 package com.idata365.app.partnerApi;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.idata365.app.remote.ChezuSmsService;
+import com.idata365.app.service.SpringContextUtil;
+
+@Component
 public class PhoneMsgTools {
-	public static boolean sendCodeMsg(String phone,String code) {
-		//发送短信验证码
-		return true;
+	public static int EventType1 = 1;// 注册
+	public static int EventType2 = 2;// 登入
+	public static int EventType3 = 3;// 找回密码
+	@Autowired
+	ChezuSmsService chezuSmsService;
+
+	public static boolean sendCodeMsg(String mobile, int templateType, String validateCode) {
+		// 发送短信验证码
+		PhoneMsgTools phoneMsgTools = SpringContextUtil.getBean("phoneMsgTools", PhoneMsgTools.class);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("mobile", mobile);
+		map.put("templateType", String.valueOf(templateType));
+		map.put("validateCode", validateCode);
+		boolean flag = phoneMsgTools.chezuSmsService.sendMessage(map);
+//		boolean flag = phoneMsgTools.chezuSmsService.sendMessageTest();
+		return flag;
 	}
 }
