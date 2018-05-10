ac25a306a22f0c01fefe9773b07f665962347e30
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-10 08:27:39 +0000

    update updatephone

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/UserInfoService.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/UserInfoService.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.idata365.app.entity.LicenseDriver;
@@ -210,7 +209,7 @@ public class UserInfoService extends BaseService<UserInfoService>{
 		UsersAccount account=new UsersAccount();
 		account.setPhone(phone);
 		account.setId(userId);
-		usersAccountMapper.updateImgUrl(account);
+		usersAccountMapper.updatePhone(account);
  
 	}
 	
