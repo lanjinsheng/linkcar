64108f59a1dc507e5c19f8783fb5d547727691e6
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-15 08:03:58 +0000

    lanjinsheng

--- chezuService/src/main/java/com/idata365/mapper/app/UserScoreDayStatMapper.java
+++ chezuService/src/main/java/com/idata365/mapper/app/UserScoreDayStatMapper.java
@@ -8,7 +8,7 @@ import com.idata365.entity.UserFamilyRoleLog;
 import com.idata365.entity.UserScoreDayStat;
 
 public interface  UserScoreDayStatMapper {
-	int updateUserScoreDayStat(UserScoreDayStat userScoreDayStat);//更新驾驶行为
+//	int updateUserScoreDayStat(UserScoreDayStat userScoreDayStat);//更新驾驶行为
 	int updateUserScoreDayById(UserScoreDayStat userScoreDayStat);//更新驾驶得分
 	
 	int insertOrUpdateUserDayStat(UserScoreDayStat userScoreDayStat);
