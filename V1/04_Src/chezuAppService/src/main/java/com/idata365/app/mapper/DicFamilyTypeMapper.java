9c2aef874d614b0240892afaf09699e055c47f84
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-16 07:49:15 +0000

    lanjinsheng v1.2

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/DicFamilyTypeMapper.java
@@ -0,0 +1,14 @@
+package com.idata365.app.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+
+import com.idata365.app.entity.DicFamilyType;
+
+public interface DicFamilyTypeMapper
+{
+	public  List<DicFamilyType>  getDicFamilyType(Map<String,Object> searchMap);
+	 
+	 
+}
