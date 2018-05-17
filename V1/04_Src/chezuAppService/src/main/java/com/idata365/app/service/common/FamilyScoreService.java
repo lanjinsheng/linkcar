667bcbc9c387d39aa1e05ea647e3968d8019f309
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-17 14:12:17 +0000

    lanjinsheng v1.2

---
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/common/FamilyScoreService.java
@@ -0,0 +1,47 @@
+package com.idata365.app.service.common;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.idata365.app.entity.UserScoreDayStat;
+import com.idata365.app.mapper.UserScoreDayStatMapper;
+
+/**
+ * 
+ * @author jinsheng
+ *
+ */
+@Service
+public class FamilyScoreService {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(FamilyScoreService.class);
+	@Autowired
+	UserScoreDayStatMapper userScoreDayStatMapper;
+
+	public Double familyScore(Long familyId, String daystamp) {
+		UserScoreDayStat pUserScoreDayStat = new UserScoreDayStat();
+		pUserScoreDayStat.setFamilyId(familyId);
+		pUserScoreDayStat.setDaystamp(daystamp);
+		List<UserScoreDayStat> list = userScoreDayStatMapper
+				.getUsersDayScoreByFamily(pUserScoreDayStat);
+		int maxUserCount = 4;
+		int i = 0;
+		Double avgScore = 0D;
+		for (UserScoreDayStat userDayScore : list) {
+			i++;
+			if (i > maxUserCount) {
+				break;
+			}
+			if (userDayScore != null) {
+
+				avgScore += userDayScore.getAvgScore();
+
+			}
+		}
+		return avgScore;
+	}
+}
