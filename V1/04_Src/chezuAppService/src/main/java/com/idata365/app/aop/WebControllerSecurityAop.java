2e08a675bfc3968d0ade1a9e49095335e8b76739
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-19 09:04:47 +0000

    lcc

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/aop/WebControllerSecurityAop.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/aop/WebControllerSecurityAop.java
@@ -24,7 +24,7 @@ import com.idata365.app.service.LoginRegService;
 import com.idata365.app.util.ResultUtils;
 
 /**
- * Created by **** 
+ * Created by ****
  */
 @Component
 @Aspect
@@ -32,64 +32,72 @@ public class WebControllerSecurityAop {
 	private final static Logger LOG = LoggerFactory.getLogger(WebControllerSecurityAop.class);
 	@Autowired
 	LoginRegService loginRegService;
-public WebControllerSecurityAop() {
-	LOG.info("WebControllerAop init");
-}
-    //匹配com.idata365.col.controller包及其子包下的所有类的所有方法
-   @Pointcut("execution(* com.idata365.app.controller..*.*(..))")
-    public Object executeService(){
+
+	public WebControllerSecurityAop() {
+		LOG.info("WebControllerAop init");
+	}
+
+	@Pointcut("execution(* com.idata365.app.controller.security..*.*(..))")
+	private void v1() {
+	}
+
+	@Pointcut("execution(* com.idata365.app.controller.securityV2..*.*(..))")
+	private void v2() {
+	}
+	// //匹配com.idata365.col.controller.security  和  securityV2包及其子包下的所有类的所有方法
+	@Pointcut("v1() || v2()")
+	public Object executeService() {
 		return "ok";
+	}
 
-    }
+	@Around("executeService()")
+	public Object doAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		LOG.info("我是需要token校验的环绕通知!!!");
+		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+		HttpServletRequest request = (HttpServletRequest) requestAttributes
+				.resolveReference(RequestAttributes.REFERENCE_REQUEST);
+		String token = request.getHeader("token");
+		LOG.info("拦截到了" + proceedingJoinPoint.getSignature().getName() + "方法...");
 
-    @Around("executeService()")
-    public Object doAroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
-    	  LOG.info("我是需要token校验的环绕通知!!!");
-    	   RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-    	     HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
-    	    String token= request.getHeader("token");
-    	   LOG.info("拦截到了" + proceedingJoinPoint.getSignature().getName() +"方法...");    
-    	   
-//	    	try {//obj之前可以写目标方法执行前的逻辑
-//	            Object obj = proceedingJoinPoint.proceed();//调用执行目标方法
-//	            return obj;
-//	        } catch (Throwable throwable) {
-//	            throwable.printStackTrace();
-//	        }
-	    	
-    	    Signature signature = proceedingJoinPoint.getSignature();    
-    	    MethodSignature methodSignature = (MethodSignature)signature;    
-    	    Method targetMethod = methodSignature.getMethod();  
-    	    if(token==null) {
-    	    	 if(targetMethod.getReturnType().getTypeName().equals("org.springframework.http.ResponseEntity")){
-    	    	    	return ResponseEntity.ok().body(ResultUtils.rtFailTokenInvalid(null, "无权限访问，请先登入!"));
-    	    	    }else {
-    	    	    	return ResultUtils.rtFailTokenInvalid(null, "无权限访问，请先登入!");
-    	    	    }
-    	    	
-    	    }
-    	    UsersAccount account=loginRegService.validToken(token);
-    	    if(account==null) {
-    	    	 if(targetMethod.getReturnType().getTypeName().equals("org.springframework.http.ResponseEntity")){
-    	    		 return ResponseEntity.ok().body(ResultUtils.rtFailTokenInvalid(null, "登入信息已经过期或者在别的设备已登入，请重新登入!"));
- 	    	    }else {
- 	    	    	return ResultUtils.rtFailTokenInvalid(null, "登入信息已经过期或者在别的设备已登入，请重新登入!");
- 	    	    }
-    	    	
-    	    }else {
-    	    	UserInfo userInfo=new UserInfo();
-    	    	userInfo.setUserAccount(account);
-    	    	request.setAttribute("userInfo", userInfo);
-    	    	try {//obj之前可以写目标方法执行前的逻辑
-    	            Object obj = proceedingJoinPoint.proceed();//调用执行目标方法
-    	            return obj;
-    	        } catch (Throwable throwable) {
-    	            throwable.printStackTrace();
-    	        }
-    	    	
-    	    }
-        return null;
-    }
-  
+		// try {//obj之前可以写目标方法执行前的逻辑
+		// Object obj = proceedingJoinPoint.proceed();//调用执行目标方法
+		// return obj;
+		// } catch (Throwable throwable) {
+		// throwable.printStackTrace();
+		// }
+
+		Signature signature = proceedingJoinPoint.getSignature();
+		MethodSignature methodSignature = (MethodSignature) signature;
+		Method targetMethod = methodSignature.getMethod();
+		if (token == null) {
+			if (targetMethod.getReturnType().getTypeName().equals("org.springframework.http.ResponseEntity")) {
+				return ResponseEntity.ok().body(ResultUtils.rtFailTokenInvalid(null, "无权限访问，请先登入!"));
+			} else {
+				return ResultUtils.rtFailTokenInvalid(null, "无权限访问，请先登入!");
+			}
+
+		}
+		UsersAccount account = loginRegService.validToken(token);
+		if (account == null) {
+			if (targetMethod.getReturnType().getTypeName().equals("org.springframework.http.ResponseEntity")) {
+				return ResponseEntity.ok().body(ResultUtils.rtFailTokenInvalid(null, "登入信息已经过期或者在别的设备已登入，请重新登入!"));
+			} else {
+				return ResultUtils.rtFailTokenInvalid(null, "登入信息已经过期或者在别的设备已登入，请重新登入!");
+			}
+
+		} else {
+			UserInfo userInfo = new UserInfo();
+			userInfo.setUserAccount(account);
+			request.setAttribute("userInfo", userInfo);
+			try {// obj之前可以写目标方法执行前的逻辑
+				Object obj = proceedingJoinPoint.proceed();// 调用执行目标方法
+				return obj;
+			} catch (Throwable throwable) {
+				throwable.printStackTrace();
+			}
+
+		}
+		return null;
+	}
+
 }
-
