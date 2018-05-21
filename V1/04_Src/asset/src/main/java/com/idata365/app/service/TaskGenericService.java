d900f18620099dfe61223ae2c0328ab238ea08b3
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-21 14:13:38 +0000

    lan add

--- V1/04_Src/asset/src/main/java/com/idata365/app/service/TaskGenericService.java
+++ V1/04_Src/asset/src/main/java/com/idata365/app/service/TaskGenericService.java
@@ -389,7 +389,7 @@ public class TaskGenericService {
 			assetUsersAssetMapper.updateDiamondsAdd(assetUsersDiamondsLogs);
 			//远程消息调用,发送的diamonds是家族获取的
 			if(familyId!=FamilyConstant.ROBOT_FAMILY_ID) {
-//			chezuAppService.sendFamilyDiamondsMsg(daystamp, String.valueOf(familyId), orderNum, assetUsersDiamondsLogs.getUserId(), String.valueOf(diamonds), sign);
+			chezuAppService.sendFamilyDiamondsMsg(daystamp, String.valueOf(familyId), orderNum, assetUsersDiamondsLogs.getUserId(), String.valueOf(diamonds), sign);
 			}
 		}
 		//family钻石减少
