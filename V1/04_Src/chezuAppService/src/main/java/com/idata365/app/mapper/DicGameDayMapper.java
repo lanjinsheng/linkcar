fe9c3820a37a847d922b6953c3a92d0654393219
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-22 05:13:06 +0000

    lanjinsheng v1.2

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/DicGameDayMapper.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/DicGameDayMapper.java
@@ -1,7 +1,5 @@
 package com.idata365.app.mapper;
 
-import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -9,7 +7,7 @@ import com.idata365.app.entity.DicGameDay;
 
 public interface DicGameDayMapper
 {
-	public  List<DicGameDay>  getDicGameDay(Map<String,Object> searchMap);
+//	public  List<DicGameDay>  getDicGameDay(Map<String,Object> searchMap);
     DicGameDay  getGameDicByDaystamp(@Param("daystamp") String daystamp);
 	 
 }
