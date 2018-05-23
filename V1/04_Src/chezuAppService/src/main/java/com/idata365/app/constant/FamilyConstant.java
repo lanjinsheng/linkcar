7a49254da688128827cdb9e38264f3c2d853c087
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-23 02:42:08 +0000

    lanjinsheng v1.2

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/constant/FamilyConstant.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/constant/FamilyConstant.java
@@ -6,8 +6,10 @@ public class FamilyConstant
 //	public static final int GOLD_TYPE = 100;
 //	//V2作废
 //	public static final int SILVER_TYPE = 90;
-//	//V2作废
-//	public static final int BRONZE_TYPE = 80;
+  //V2作废
+  //public static final int BRONZE_TYPE = 80;
 	public static final Double USER_LOGIN_SCORE=30D;
 	public static final int ROBOT_FAMILY_ID = 999999;
+	//家族人数
+	public static final int FAMILY_TOTAL_NUM=6;
 }
