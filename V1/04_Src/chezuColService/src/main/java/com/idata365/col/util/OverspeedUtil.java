4c94df6848941f1a574acbdfccfed051eedee70c
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-23 04:46:31 +0000

    lanjinsheng add

--- V1/04_Src/chezuColService/src/main/java/com/idata365/col/util/OverspeedUtil.java
+++ V1/04_Src/chezuColService/src/main/java/com/idata365/col/util/OverspeedUtil.java
@@ -1,18 +1,55 @@
 package com.idata365.col.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class OverspeedUtil {
 	public static final BigDecimal kmBei=BigDecimal.valueOf(3.6);
 	public static final BigDecimal overBei=BigDecimal.valueOf(1.1);
 	public static final int filterDist1=200;
 	public static final int filterDist2=300;
-	public static final int filterCount=4;;
+	public static final int filterCount=4;
 	
+	
+	public String readToString(String fileName) {  
+        String encoding = "UTF-8";  
+        File file = new File(fileName);  
+        Long filelength = file.length();  
+        byte[] filecontent = new byte[filelength.intValue()];  
+        try {  
+            FileInputStream in = new FileInputStream(file);  
+            in.read(filecontent);  
+            in.close();  
+        } catch (FileNotFoundException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+        try {  
+            return new String(filecontent, encoding);  
+        } catch (UnsupportedEncodingException e) {  
+            System.err.println("The OS does not support " + encoding);  
+            e.printStackTrace();  
+            return null;  
+        }  
+    }
+	
+	public static void main(String []args) {
+//		OverspeedUtil util= new OverspeedUtil();
+//		String gps=util.readToString("D:\\jsonGps");
+//		Map<String,Object> jsonMap=GsonUtils.fromJson(gps);
+//		Object overspeedInfo=jsonMap.get("overspeedGPSInfo");
+//		List<Map<String,Object>> list=util.dealOverSpeed(overspeedInfo);
+	}
 	/**
 	 * 
 	    * @Title: dealOverSpeed
@@ -23,15 +60,12 @@ public class OverspeedUtil {
 	    * @throws
 	    * @author LanYeYe
 	 */
-	public static List<Map<String,Object>> dealOverSpeed(Object overspeedInfo){
-		if(overspeedInfo==null) {
-			return null;
-		}
-		List<Map<String,Object>> jkList=(List<Map<String,Object>>)overspeedInfo;
-		if(jkList==null || jkList.size()==0) {
-			return null;
-		}
+	public static List<Map<String,Object>> dealOverSpeed(List<Map<String,Object>> jkList){
 		List<Map<String,Object>> overSpeedArray=new ArrayList<Map<String,Object>>();
+		if(jkList==null || jkList.size()==0) {
+			return overSpeedArray;
+		}
+		
 		for(Map<String,Object> map:jkList) {
 			String jkGps=String.valueOf(map.get("jk"));
 			List<Map<String,Object>> jkPoints=(List<Map<String,Object>>)map.get("set");
@@ -41,10 +75,10 @@ public class OverspeedUtil {
 				double d=Double.valueOf(point.get("d").toString());
 				double ls=Double.valueOf(point.get("ls").toString());
 				double s=Double.valueOf(point.get("s").toString());
-				if(s<=18 && d>filterDist1) {
-					continue;
-				}
-                if(s>18 && d>filterDist2) {
+//				if(s<=18 && d>filterDist1) {
+//					continue;
+//				}
+                if(d>filterDist2) {
                 	continue;
                 }
 				String t=String.valueOf(point.get("t"));
