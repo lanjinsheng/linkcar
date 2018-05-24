1be4bc8cf13123fa33f8b34f8558ec0e7fe15723
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-24 11:56:12 +0000

    lcc update

--- chezuAppService/src/main/java/com/idata365/app/constant/IndexSlipPage.java
+++ chezuAppService/src/main/java/com/idata365/app/constant/IndexSlipPage.java
@@ -2,10 +2,13 @@ package com.idata365.app.constant;
 
 public class IndexSlipPage
 {
-	public static final String ONE = "http://apph5.idata365.com/appImgs/hongbao50.png";
+	public static final String ONE = "http://apph5.idata365.com/appImgs/SilpPage1.png";
 	
-	public static final String TWO = "http://apph5.idata365.com/appImgs/hongbao25.png";
+	public static final String TWO = "http://apph5.idata365.com/appImgs/SilpPage2.png";
 	
-	public static final String THREE = "http://apph5.idata365.com/appImgs/hongbao10.png";
+	public static final String THREE = "http://apph5.idata365.com/appImgs/SilpPage3.png";
+	
+	public static final String FOUR = "http://apph5.idata365.com/appImgs/SilpPage4.png";
 	
 }
+
