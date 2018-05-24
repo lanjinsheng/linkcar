3c696e28d79a8e56ff26d5bfd4eb7bc6611e4862
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-24 13:38:19 +0000

    lanjinsheng v1.2.1

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/constant/IndexSlipPage.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/constant/IndexSlipPage.java
@@ -2,13 +2,13 @@ package com.idata365.app.constant;
 
 public class IndexSlipPage
 {
-	public static final String ONE = "http://apph5.idata365.com/appImgs/SilpPage1.png";
+	public static final String ONE = "http://product-h5.idata365.com/appImgs/start/1.png";
 	
-	public static final String TWO = "http://apph5.idata365.com/appImgs/SilpPage2.png";
+	public static final String TWO = "http://product-h5.idata365.com/appImgs/start/2.png";
 	
-	public static final String THREE = "http://apph5.idata365.com/appImgs/SilpPage3.png";
+	public static final String THREE = "http://product-h5.idata365.com/appImgs/start/3.png";
 	
-	public static final String FOUR = "http://apph5.idata365.com/appImgs/SilpPage4.png";
+	public static final String FOUR = "http://product-h5.idata365.com/appImgs/start/4.png";
 	
 }
 
