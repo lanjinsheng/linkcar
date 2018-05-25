a596bfedd2b44af5b72db7da66cadd82bdbee1b8
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-25 01:40:31 +0000

    lcc update

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/DicService.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/service/DicService.java
@@ -57,7 +57,8 @@ public class DicService extends BaseService<DicService> {
 
 		long day = (date2.getTime() - date1.getTime()) / (24 * 60 * 60 * 1000);
 
-		return "距离"+endDay+"赛季结束还剩："+day + "天";
+//		return "距离"+endDay+"赛季结束还剩："+day + "天";
+		return day+"";
 	}
 
 	private String getCurrentDayStr() {
