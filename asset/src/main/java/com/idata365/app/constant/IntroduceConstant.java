3f808c4dbd701f9d02272027b0f2d822e6435811
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-05-31 14:28:01 +0000

    lcc update

--- asset/src/main/java/com/idata365/app/constant/IntroduceConstant.java
+++ asset/src/main/java/com/idata365/app/constant/IntroduceConstant.java
@@ -2,8 +2,8 @@ package com.idata365.app.constant;
 
 public class IntroduceConstant {
 	public static final String DIAMONDINTRODUCE = "钻石是基于区块链技术，通过智能合约来每日发放的奖励，可以用于好车族内的商品竞拍或奖品兑换等。\n"
-			+ "钻石每日固定生产1000颗，可以通过个人动力积攒、家族对战获胜、赛季结束奖励三种方式来挖据。";
+			+ "钻石每日固定生产1000颗，可以通过个人动力积攒、家族对战获胜、赛季结束奖励三种方式来挖掘。";
 
-	public static final String POWERINTRODUCE = "动力是用来挖据钻石的重要元素，每日挖掘产生的钻石会有一部分根据个人今日积攒的动力在全网中的占比来分配给每个玩家。\n"
+	public static final String POWERINTRODUCE = "动力是用来挖掘钻石的重要元素，每日挖掘产生的钻石会有一部分根据个人今日积攒的动力在全网中的占比来分配给每个玩家。\n"
 			+ "玩家可以通过驾驶行为评分获取、停车场收取对战动力、点击主页概率生成的附有动力值的小车领取，三种方式来积攒动力。动力会在次日0点清空并自动按比例兑换成钻石。";
 }
