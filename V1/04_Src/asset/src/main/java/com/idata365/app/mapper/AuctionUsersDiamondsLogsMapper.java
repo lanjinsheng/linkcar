f008599f9f68574fd931ade99b2d16364cedea83
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-13 14:18:29 +0000

    lcc v1.5

--- V1/04_Src/asset/src/main/java/com/idata365/app/mapper/AuctionUsersDiamondsLogsMapper.java
+++ V1/04_Src/asset/src/main/java/com/idata365/app/mapper/AuctionUsersDiamondsLogsMapper.java
@@ -1,9 +1,12 @@
 package com.idata365.app.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.idata365.app.entity.AuctionUsersDiamondsLogs;
 
 public interface AuctionUsersDiamondsLogsMapper {
 
 	int insertDiamondsConsume(AuctionUsersDiamondsLogs auctionUsersDiamondsLogs);
 
+	AuctionUsersDiamondsLogs getAuctionUsersDiamondsLogsById(@Param("preUserId") long preUserId, @Param("auctionGoodsId") long auctionGoodsId);
 }
