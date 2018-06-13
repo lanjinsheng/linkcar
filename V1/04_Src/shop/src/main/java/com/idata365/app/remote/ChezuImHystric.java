1951968391d8d5dc3e2285e96358bd42363f7ce4
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-13 03:43:49 +0000

    初始导入。

---
+++ V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuImHystric.java
@@ -0,0 +1,26 @@
+package com.idata365.app.remote;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.idata365.app.entity.bean.AuctionBean;
+
+@Component
+public class ChezuImHystric implements ChezuImService {
+	private final static Logger LOG = LoggerFactory.getLogger(ChezuImHystric.class);
+
+	@Override
+	public Map<String,Object>  notifyAuction(@RequestBody  AuctionBean auctionBean){
+	// TODO Auto-generated method stub
+		LOG.error("notifyAuction 挂了");
+		return null;
+	}
+
+ 
+ 
+
+}
