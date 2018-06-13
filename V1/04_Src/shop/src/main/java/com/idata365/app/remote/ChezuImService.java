9411c0cfa8053584fa32764bb34f4214f98d69bc
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-13 03:45:11 +0000

    初始导入。

--- V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuImService.java
+++ V1/04_Src/shop/src/main/java/com/idata365/app/remote/ChezuImService.java
@@ -1,16 +1,13 @@
 package com.idata365.app.remote;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.idata365.app.entity.AuctionGoods;
-import com.idata365.app.entity.AuctionLogs;
+ 
 import com.idata365.app.entity.bean.AuctionBean;
 
 /**
