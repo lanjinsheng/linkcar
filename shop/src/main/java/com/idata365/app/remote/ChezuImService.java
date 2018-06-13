e85d0b707a5c22a2d750a8f43b0c2dd939102e81
lanjinsheng <lanjinsheng@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-13 03:43:49 +0000

    初始导入。

---
+++ shop/src/main/java/com/idata365/app/remote/ChezuImService.java
@@ -0,0 +1,29 @@
+package com.idata365.app.remote;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.idata365.app.entity.AuctionGoods;
+import com.idata365.app.entity.AuctionLogs;
+import com.idata365.app.entity.bean.AuctionBean;
+
+/**
+ * 
+ * @ClassName: ChezuImService
+ * @Description: TODO()
+ * @author LanYeYe
+ * @date 2017年12月28日
+ *
+ */
+@FeignClient(value = "service-im-chezu-ljs", fallback = ChezuImHystric.class)
+public interface ChezuImService {
+	@RequestMapping(value = "/im/notifyAuction",method = RequestMethod.POST)
+	public Map<String,Object>  notifyAuction(@RequestBody  AuctionBean auctionBean);
+	
+}
