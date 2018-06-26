51bbfcca1c8a6bca48a570af8e2f96c4af84037c
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-26 05:01:13 +0000

    lcc v1.6

--- V1/04_Src/asset/src/main/java/com/idata365/app/remote/ChezuAppService.java
+++ V1/04_Src/asset/src/main/java/com/idata365/app/remote/ChezuAppService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.idata365.app.entity.UsersAccount;
+import com.idata365.app.util.DateTools;
 
 /**
  * 
@@ -19,51 +20,68 @@ import com.idata365.app.entity.UsersAccount;
  */
 @FeignClient(value = "service-app-chezu", fallback = ChezuAppHystric.class)
 public interface ChezuAppService {
-	
-    /**
-     * 
-        * @Title: sendFamilyDiamondsMsg
-        * @Description: TODO(钻石分配通知)
-        * @param @param season
-        * @param @param familyId
-        * @param @param orderNum
-        * @param @param toUserId
-        * @param @param diamondNum
-        * @param @param sign
-        * @param @return    参数
-        * @return boolean    返回类型
-        * @throws
-        * @author LanYeYe
-     */
-    @RequestMapping("/app/msg/sendFamilyDiamondsMsg")
-    public boolean sendFamilyDiamondsMsg(@RequestParam (value = "season") String season,
-    		@RequestParam (value = "familyId") String familyId,
-    		@RequestParam (value = "orderNum") String orderNum,
-    		@RequestParam (value = "toUserId") Long toUserId,
-    		@RequestParam (value = "diamondNum") String diamondNum,
-    		@RequestParam (value = "personDiamondNum") String personDiamondNum,
-    		@RequestParam (value = "sign") String sign);
-   
-    @RequestMapping("/app/msg/sendFamilyPowerMsg")
-    public boolean sendFamilyPowerMsg(@RequestParam (value = "season") String season,
-    		@RequestParam (value = "familyId") String familyId,
-    		@RequestParam (value = "orderNum") String orderNum,
-    		@RequestParam (value = "toUserId") Long toUserId,
-    		@RequestParam (value = "PowerNum") String powerNum,
-    		@RequestParam (value = "personPowerNum") String personPowerNum,
-    		@RequestParam (value = "sign") String sign);
-    
-    
-    @RequestMapping("/app/msg/sendFamilyDiamondsSeasonMsg")
-    public boolean sendFamilyDiamondsSeasonMsg(@RequestParam (value = "season") String season,
-    		@RequestParam (value = "familyId") String familyId,
-    		@RequestParam (value = "familyType") Integer familyType,
-    		@RequestParam (value = "toUserId") Long toUserId,
-    		@RequestParam (value = "diamondNum") String diamondNum,
-    		@RequestParam (value = "personDiamondNum") String personDiamondNum,
-    		@RequestParam (value = "sign") String sign);
-	
-	@RequestMapping(value = "/app/updateLoginBss",method = RequestMethod.POST)
-	public boolean  updateLoginBss(@RequestParam(value="userId") long userId,@RequestParam(value="sign") String sign);
 
+	/**
+	 * 
+	 * @Title: sendFamilyDiamondsMsg
+	 * @Description: TODO(钻石分配通知)
+	 * @param @param
+	 *            season
+	 * @param @param
+	 *            familyId
+	 * @param @param
+	 *            orderNum
+	 * @param @param
+	 *            toUserId
+	 * @param @param
+	 *            diamondNum
+	 * @param @param
+	 *            sign
+	 * @param @return
+	 *            参数
+	 * @return boolean 返回类型
+	 * @throws @author
+	 *             LanYeYe
+	 */
+	@RequestMapping("/app/msg/sendFamilyDiamondsMsg")
+	public boolean sendFamilyDiamondsMsg(@RequestParam(value = "season") String season,
+			@RequestParam(value = "familyId") String familyId, @RequestParam(value = "orderNum") String orderNum,
+			@RequestParam(value = "toUserId") Long toUserId, @RequestParam(value = "diamondNum") String diamondNum,
+			@RequestParam(value = "personDiamondNum") String personDiamondNum,
+			@RequestParam(value = "sign") String sign);
+
+	@RequestMapping("/app/msg/sendFamilyPowerMsg")
+	public boolean sendFamilyPowerMsg(@RequestParam(value = "season") String season,
+			@RequestParam(value = "familyId") String familyId, @RequestParam(value = "orderNum") String orderNum,
+			@RequestParam(value = "toUserId") Long toUserId, @RequestParam(value = "PowerNum") String powerNum,
+			@RequestParam(value = "personPowerNum") String personPowerNum, @RequestParam(value = "sign") String sign);
+
+	@RequestMapping("/app/msg/sendFamilyDiamondsSeasonMsg")
+	public boolean sendFamilyDiamondsSeasonMsg(@RequestParam(value = "season") String season,
+			@RequestParam(value = "familyId") String familyId, @RequestParam(value = "familyType") Integer familyType,
+			@RequestParam(value = "toUserId") Long toUserId, @RequestParam(value = "diamondNum") String diamondNum,
+			@RequestParam(value = "personDiamondNum") String personDiamondNum,
+			@RequestParam(value = "sign") String sign);
+
+	@RequestMapping(value = "/app/updateLoginBss", method = RequestMethod.POST)
+	public boolean updateLoginBss(@RequestParam(value = "userId") long userId,
+			@RequestParam(value = "sign") String sign);
+
+	/**
+	 * 
+	 * @Title: getFightRelationAsset
+	 * @Description: TODO(用于从app获取对战家族ID和人数，临时使用)
+	 * @param @param
+	 *            familyId
+	 * @param @param
+	 *            sign
+	 * @param @return
+	 *            参数
+	 * @return Map<String,Object> 返回类型
+	 * @throws @author
+	 *             LiXing
+	 */
+	@RequestMapping("/app/getFightRelationAsset")
+	public Map<String, Object> getFightRelationAsset(@RequestParam(value = "familyId") Long familyId,
+			@RequestParam(value = "sign") String sign);
 }
