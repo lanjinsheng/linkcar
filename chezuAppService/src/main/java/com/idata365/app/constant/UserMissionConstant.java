86d32a89ff992a89a495e3663fd38964d6582232
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-06-27 12:09:41 +0000

    lcc v1.6

--- chezuAppService/src/main/java/com/idata365/app/constant/UserMissionConstant.java
+++ chezuAppService/src/main/java/com/idata365/app/constant/UserMissionConstant.java
@@ -22,19 +22,19 @@ public class UserMissionConstant {
 	// 创建一个俱乐部
 	public static final int CREATREHOME_ID = 9;
 	// 创建的俱乐部达到白银
-	public static final int SILVER_ID = 9;
+	public static final int SILVER_ID = 10;
 	// 创建的俱乐部达到黄金
-	public static final int GOLD_ID = 9;
+	public static final int GOLD_ID = 11;
 	// 创建的俱乐部达到钻石
-	public static final int DIAMOND_ID = 9;
+	public static final int DIAMOND_ID = 12;
 	// 创建的俱乐部达到冠军
-	public static final int CHAMPION_ID = 9;
+	public static final int CHAMPION_ID = 13;
 	// 单次行程10公里以上时获得100分评分
-	public static final int SCORE_ID = 9;
+	public static final int SCORE_ID = 14;
 	// 累计里程500公里
-	public static final int KM500_ID = 9;
+	public static final int KM500_ID = 15;
 	// 连续5次行程获得100分
-	public static final int SCORETIMES5_ID = 9;
+	public static final int SCORETIMES5_ID = 16;
 	
 	
 	 /**
