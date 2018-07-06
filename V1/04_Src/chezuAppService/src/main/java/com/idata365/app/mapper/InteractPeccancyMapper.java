a8c3dd651f1cb39f2cd132d3a62817bf73ac05f1
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-06 08:16:48 +0000

    lcc v1.7

--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/InteractPeccancyMapper.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/InteractPeccancyMapper.java
@@ -1,16 +1,15 @@
 package com.idata365.app.mapper;
 
-
-
 import org.apache.ibatis.annotations.Param;
 
 import com.idata365.app.entity.InteractPeccancy;
 
-
 public interface InteractPeccancyMapper {
 	int insertPeccancy(InteractPeccancy interactPeccancy);
-	
+
 	int updatePeccancy(InteractPeccancy interactPeccancy);
-	
+
 	InteractPeccancy getPeccancy(@Param("id") Long id);
+
+	int isCanPayTicket(@Param("userId") long userId);
 }
--- V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/InteractTempCarMapper.java
+++ V1/04_Src/chezuAppService/src/main/java/com/idata365/app/mapper/InteractTempCarMapper.java
@@ -7,16 +7,19 @@ import org.apache.ibatis.annotations.Param;
 
 import com.idata365.app.entity.InteractTempCar;
 
-
 public interface InteractTempCarMapper {
 
 	int lockCar(InteractTempCar interactTempCar);
 
 	List<InteractTempCar> getTempCar(@Param("uuid") String uuid);
+
 	InteractTempCar getTempCarByUuid(@Param("uuid") String uuid);
-	int batchInsertCar(List<Map<String,Object>> list);
-	
-	int updateTempCar(Map<String,Object> map);
-	
-	int updateBlackIds(Map<String,Object> map);
+
+	int batchInsertCar(List<Map<String, Object>> list);
+
+	int updateTempCar(Map<String, Object> map);
+
+	int updateBlackIds(Map<String, Object> map);
+
+	int isCanStealPower(@Param("userId") long userId);
 }
