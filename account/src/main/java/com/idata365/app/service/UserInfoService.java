4d3aa50eba99a75eeac59b8639b4272bd667735e
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-10 03:15:48 +0000

    lcc v1.7

--- account/src/main/java/com/idata365/app/service/UserInfoService.java
+++ account/src/main/java/com/idata365/app/service/UserInfoService.java
@@ -276,7 +276,9 @@ public class UserInfoService extends BaseService<UserInfoService> {
 	
 	public int queryIsCanJoinMe(long userId) {
 		int i = 1;
-		if(userConfigMapper.queryIsCanJoinMe(userId)!=null) {
+		if (userConfigMapper.queryIsCanJoinMe(userId) != null) {
+			i = -1;
+		} else {
 			i = userConfigMapper.queryIsCanJoinMe(userId);
 		}
 		return i;
