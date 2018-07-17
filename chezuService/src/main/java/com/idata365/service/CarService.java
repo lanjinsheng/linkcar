409dcdb2da41a4f3e89bd37260205b4c92068000
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-17 09:26:49 +0000

    lcc v2.0

---
+++ chezuService/src/main/java/com/idata365/service/CarService.java
@@ -0,0 +1,45 @@
+package com.idata365.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.idata365.mapper.app.UserCarMapper;
+
+/**
+ * 
+        * @ClassName: CarService
+        * @Description: TODO(car)
+        * @author LiXing
+        * @date 2018年7月17日
+        *
+ */
+@Service
+public class CarService extends BaseService<CarService> {
+	private final static Logger LOG = LoggerFactory.getLogger(CarService.class);
+	@Autowired
+	UserCarMapper userCarMapper;
+
+	public CarService() {
+
+	}
+	
+	/**
+	 * 
+        * @Title: sendUserCarAndCarIdIs2
+        * @Description: TODO(所创建家族等级达到黄金时候奖励CarID为2的车)
+        * @param  参数
+        * @return void 返回类型
+        * @throws
+        * @author LiXing
+	 */
+	@Transactional
+	public void sendUserCarAndCarIdIs2(long userId) {
+		LOG.info("userId=============================" + userId);
+		this.userCarMapper.sendUserCarAndCarIdIs2(userId);
+	}
+	
+}
