f10440bad890d642c2bcf60f26e1dd55b132b6d3
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-18 03:19:52 +0000

    lcc update

--- V1/04_Src/chezuService/src/main/java/com/idata365/constant/DicFamilyTypeConstant.java
+++ V1/04_Src/chezuService/src/main/java/com/idata365/constant/DicFamilyTypeConstant.java
@@ -43,6 +43,21 @@ public class DicFamilyTypeConstant {
     public  final static Integer GuanJun_3=122;
     public  final static Integer GuanJun_4=123;
     public  final static Integer GuanJun_5=124;
+    public  final static Integer GuanJun_6=125;
+    public  final static Integer GuanJun_7=126;
+    public  final static Integer GuanJun_8=127;
+    public  final static Integer GuanJun_9=128;
+    public  final static Integer GuanJun_10=129;
+    public  final static Integer GuanJun_11=130;
+    public  final static Integer GuanJun_12=131;
+    public  final static Integer GuanJun_13=132;
+    public  final static Integer GuanJun_14=133;
+    public  final static Integer GuanJun_15=134;
+    public  final static Integer GuanJun_16=135;
+    public  final static Integer GuanJun_17=136;
+    public  final static Integer GuanJun_18=137;
+    public  final static Integer GuanJun_19=138;
+    public  final static Integer GuanJun_20=139;
     
     
 	public static DicFamilyType getDicFamilyType(Integer familyType){
@@ -57,11 +72,40 @@ public class DicFamilyTypeConstant {
 			
 	}
 	
-	 
 	
 	public static Integer getDicFamilyTypeByTrophy(Integer trophy){
 		  if(trophy>=250) {
-			  if(trophy>=880) {
+			  if(trophy>=1330) {
+				  return GuanJun_20;
+			  }else if(trophy>=1270) {
+				  return GuanJun_19;
+			  }else if(trophy>=1240) {
+				  return GuanJun_18;
+			  }else if(trophy>=1210) {
+				  return GuanJun_17;
+			  }else if(trophy>=1180) {
+				  return GuanJun_16;
+			  }else if(trophy>=1150) {
+				  return GuanJun_15;
+			  }else if(trophy>=1120) {
+				  return GuanJun_14;
+			  }else if(trophy>=1090) {
+				  return GuanJun_13;
+			  }else if(trophy>=1060) {
+				  return GuanJun_12;
+			  }else if(trophy>=1030) {
+				  return GuanJun_11;
+			  }else if(trophy>=1000) {
+				  return GuanJun_10;
+			  }else if(trophy>=970) {
+				  return GuanJun_9;
+			  }else if(trophy>=940) {
+				  return GuanJun_8;
+			  }else if(trophy>=910) {
+				  return GuanJun_7;
+			  }else if(trophy>=850) {
+				  return GuanJun_6;
+			  }else if(trophy>=880) {
 				  return GuanJun_5;
 			  }else if(trophy>=850) {
 				  return GuanJun_4;
