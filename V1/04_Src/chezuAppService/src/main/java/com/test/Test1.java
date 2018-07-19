1f9ae6579a7b76c39e346bcc141e586328c58627
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-19 02:10:58 +0000

    lcc update

--- V1/04_Src/chezuAppService/src/main/java/com/test/Test1.java
+++ V1/04_Src/chezuAppService/src/main/java/com/test/Test1.java
@@ -9,53 +9,62 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class Test1
-{
+public class Test1 {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Test1.class);
-	
-	public static void main(String[] args)
-	{
-System.out.print(Boolean.valueOf(null));
+
+	public static void main(String[] args) {
+		// Person person = new Person();
+		// person.setAge(5);
+		//
+		// People people = new People();
+		// AdBeanUtils.copyOtherPropToStr(people, person);
+		// System.out.println(JSON.toJSONString(people));
+
+		// test2UUID();
+
+		// test1();
+
+		// BigDecimal.valueOf(100).divide(null, 0, 0)
+
+		// calBatch(100, 3);
+
+		// int generateRand = RandUtils.generateRand(1, 200);
+
+		hidePhone();
 	}
 
-	public static String hidePhone()
-	{
+	public static String hidePhone() {
 		String phone = "18795860371";
 		String prefixPhone = StringUtils.substring(phone, 0, 3);
 		String suffixPhone = StringUtils.substring(phone, 7);
-		
+
 		String tempNewPhone = prefixPhone + "****" + suffixPhone;
 		return tempNewPhone;
-//		LOGGER.info(tempUserName);
+		// LOGGER.info(tempUserName);
 	}
-	
-	public static void calBatch(int totalNum, int batchNUm)
-	{
+
+	public static void calBatch(int totalNum, int batchNUm) {
 		BigDecimal totalNumBd = BigDecimal.valueOf(totalNum);
 		BigDecimal batchNUmBd = BigDecimal.valueOf(batchNUm);
 		BigDecimal resultBd = totalNumBd.divide(batchNUmBd, 0, BigDecimal.ROUND_HALF_UP);
 		System.out.println("--------------" + resultBd.toString());
 	}
-	
-	public static void test2UUID()
-	{
+
+	public static void test2UUID() {
 		Set<String> inviteSet = new HashSet<>();
-		
-		for (int i = 0; i < 100; i++)
-		{
+
+		for (int i = 0; i < 100; i++) {
 			UUID uuid = UUID.randomUUID();
 			String uuidStr = uuid.toString();
 			String inviteCode = StringUtils.substring(uuidStr, 0, 8).toUpperCase();
 			inviteSet.add(inviteCode);
 		}
-		
+
 		LOGGER.info("size==={}", inviteSet.size());
 		LOGGER.info(inviteSet.toString());
 	}
-	
-	public static void test1()
-	{
+
+	public static void test1() {
 		String givenTime = "20171221122122";
 		String dayStr = StringUtils.substring(givenTime, 0, 8);
 		System.out.println(dayStr);
