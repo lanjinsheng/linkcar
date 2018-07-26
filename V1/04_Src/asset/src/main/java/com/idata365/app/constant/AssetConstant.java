351ba4d2a2af26cbab9d86a266d928537ef63821
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-07-26 07:06:22 +0000

    lcc update

--- V1/04_Src/asset/src/main/java/com/idata365/app/constant/AssetConstant.java
+++ V1/04_Src/asset/src/main/java/com/idata365/app/constant/AssetConstant.java
@@ -55,7 +55,8 @@ public class AssetConstant {
 	public final static int EVENTTYPE_POWER_MISSION_SCORE_ID = 114; // 单次行程10公里以上时获得100分评分
 	public final static int EVENTTYPE_POWER_MISSION_KM500_ID = 115; // 累计里程500公里
 	public final static int EVENTTYPE_POWER_MISSION_SCORETIMES5_ID = 116; // 连续5次行程获得100分
-	public final static int EVENTTYPE_POWER_MISSION_AD = 117; // 广告任务
+	public final static int EVENTTYPE_POWER_MISSION_AD = 117; // 观看广告
+	public final static int EVENTTYPE_POWER_MISSION_INTERACTAD = 118; // 参与活动
 	//个人钻石表
 	public final static int EVENTTYPE_DAIMOND_DAYPOWER_USER = 1;// 每日分配
 	public final static int EVENTTYPE_DAIMOND_GAMEEND_USER = 2;// PK结束家族分配
@@ -129,8 +130,8 @@ public class AssetConstant {
 		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_SCORE_ID, "完成任务-稳定驾驶");
 		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_KM500_ID, "完成任务-一路相伴");
 		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_SCORETIMES5_ID, "完成任务-满分司机");
-		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_AD, "完成任务-观看广告一次");
-		
+		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_AD, "观看广告奖励");
+		UserPowerEventMap.put(EVENTTYPE_POWER_MISSION_INTERACTAD, "完成活动奖励");
 
 	}
 }
