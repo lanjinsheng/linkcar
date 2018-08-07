123cc015552ba3a1d6cd2b6f834aca3930f4300c
lixing <lixing@ba625775-5f01-0010-890b-93b6b773ee2d>
2018-08-07 01:46:18 +0000

    lcc v2.1

--- V1/04_Src/asset/src/main/java/com/idata365/app/constant/IntroduceConstant.java
+++ V1/04_Src/asset/src/main/java/com/idata365/app/constant/IntroduceConstant.java
@@ -1,9 +1,9 @@
 package com.idata365.app.constant;
 
 public class IntroduceConstant {
-	public static final String DIAMONDINTRODUCE = "钻石是基于区块链技术，通过每个人的驾驶行为评估以及互动来每日发放的奖励，可以用于链车内的奖品兑换等。\n"
-			+ "钻石每日固定生产1000颗，可以通过个人动力积攒、俱乐部对战获胜、赛季结束奖励三种方式来挖掘。";
+	public static final String DIAMONDINTRODUCE = "钻石是用户通过驾驶行为以及在链车社区中的互动行为产生的动力兑换而成的虚拟价值货币。可用于链车内奖品兑换实际价值。\n"
+			+ "链车全网每日固定产生钻石1000个。";
 
-	public static final String POWERINTRODUCE = "动力是用来挖掘钻石的重要元素，每日挖掘产生的钻石会有一部分根据个人今日积攒的动力在全网中的占比来分配给每个玩家。\n"
-			+ "玩家可以通过驾驶行为评分获取、停车场收取对战动力、点击主页概率生成的附有动力值的小车领取，三种方式来积攒动力。动力会在次日0点清空并自动按比例兑换成钻石。";
+	public static final String POWERINTRODUCE = "动力是用来挖掘钻石的唯一元素。\n"
+			+ "每日获取的动力在全网中的动力占比越多，获取的钻石也就相对越多。次日0点时，玩家当日所获得的钻石会根据获取的动力在全网中的占比自动分配。";
 }
